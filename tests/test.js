37e4965c3ecab8b3bddd667976739fe753b9589e
Michael Gamlem III <mgamlem3@gmail.com>
2020-12-11 17:16:14 -0800

    add tests

---
+++ tests/test.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const express = require("express");
+const chai = require("chai");
+const request = require("supertest");
+const { assert } = require("console");
+
+const app = express();
+
+describe("GET /api", () => {
+	it("should return 501", () => {
+		request(app).get("/api").send().expect(501);
+	});
+});
+
+describe("GET /api/weather/city/bellingham", () => {
+	it("should return weather", () => {
+		request(app).get("/api/weather/city/bellingham").send().expect(200).then((res) => {
+			assert(res.body);
+		})
+	})
+});
